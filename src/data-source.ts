5a2f88bccdc10aa704376b78790cc5eb50078328
Vinicius <vinicius.stmartins@gmail.com>
2022-10-26 16:31:03 -0300

    ignorar

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  synchronize: true,
+  synchronize: false,
   entities: ["src/entities/**.entity.ts"],
-  migrations: ["migrations/**/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
 });
