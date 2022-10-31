3ccdf1cea07f2d0b4a1f7826de653dde7f3b5ea9
Vinicius <vinicius.stmartins@gmail.com>
2022-10-31 10:22:35 -0300

    heroku
    Signed-off-by: Vinicius <vinicius.stmartins@gmail.com>

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appRoutes = void 0;
-const veicules_routes_1 = require("./veicules.routes");
-const appRoutes = (app) => {
-    app.use("/veicules", (0, veicules_routes_1.veiculeRoutes)());
-};
-exports.appRoutes = appRoutes;
