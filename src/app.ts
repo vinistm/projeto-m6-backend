1aff17af7a8f4dd2f2baa4d045f244c52db3a058
Vinicius <vinicius.stmartins@gmail.com>
2022-11-03 10:19:11 -0300

    heroku : cors
    Signed-off-by: Vinicius <vinicius.stmartins@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import express from "express";
 import "reflect-metadata";
 import { appRoutes } from "./routes";
 import handleError from "./middlewares/handleError";
+import cors from "cors"
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 appRoutes(app);
 
